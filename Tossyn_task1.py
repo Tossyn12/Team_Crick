c77b583230a3d10d2a09e8c2a0f6ce0f287820c4
Tossyn12 <88286746+Tossyn12@users.noreply.github.com>
2021-10-16 14:04:50 +0100

    Add files via upload

---
+++ Tossyn_task1.py
@@ -0,0 +1,8 @@
+name = "Adetobi Tosin"
+email = "tossynemmanuel150@gmail.com"
+slack_username = "@Tossyn"
+biostack = "Drug development"
+output = name + ', ' + email + ', ' + slack_username + ', ' + biostack
+file = open('C:/Users/Tossyn/Desktop/Tossyn/output.txt', 'w')
+file.write(output)
+file.close()
